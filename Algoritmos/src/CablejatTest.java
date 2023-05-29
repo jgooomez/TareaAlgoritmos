74a156637fc2ee81731f0e96aa5c078f3ace5d7d
dam_gdj <javierillo756@gmail.com>
2023-05-29 12:00:24 +0200

    Clase cableado test terminada

--- Algoritmos/src/CablejatTest.java
+++ Algoritmos/src/CablejatTest.java
@@ -49,4 +49,38 @@ class CablejatTest {
 
         assertEquals(esperado,actual);
     }
+
+    @Test
+    void testEsPotConectarCorrecto() {
+        ArrayList<String> cables = new ArrayList<>();
+        cables.add("FF");
+        cables.add("MM");
+        cables.add("MM");
+        cables.add("FF");
+        cables.add("FM");
+        cables.add("MF");
+
+        boolean actual = c.esPotConectar(cables);
+        boolean esperado = true;
+
+        assertEquals(esperado,actual);
+    }
+    @Test
+    void testEsPotConectarVariosTipos() {
+        ArrayList<String> cables = new ArrayList<>();
+        cables.add("FF");
+        cables.add("MM");
+        cables.add("FF");
+        cables.add("MM");
+        cables.add("MF");
+        cables.add("MF");
+        cables.add("MM");
+        cables.add("FF");
+
+        boolean actual = c.esPotConectar(cables);
+        boolean esperado = true;
+
+        assertEquals(esperado,actual);
+    }
+
 }
